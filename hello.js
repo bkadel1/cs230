ff9b523c1fb9c95a56d727935c5083342dc49d47
bkadel1 <144166993+bkadel1@users.noreply.github.com>
2023-11-01 20:32:46 -0400

    added new page 9 and java script

--- hello.js
+++ hello.js
@@ -1,13 +1,10 @@
-// hello.js
 
-// Declare a global variable clickCount and initialize it to 0
 var clickCount = 0;
 
-// Define a function named hello
 function hello() {
-  // Increment clickCount
+  
   clickCount++;
 
-  // Display a popup alert
+  
   alert("You clicked the button " + clickCount + " times");
 }
